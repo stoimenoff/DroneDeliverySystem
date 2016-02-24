d35efb610c662c435fe4238dc50032fdef948926
stoimenoff <stoimenoffjr@gmail.com>
2016-02-24 22:57:22 +0200

    Drone make delivery + estimate time added

--- DroneDeliverySystem/src/drone/Drone.java
+++ DroneDeliverySystem/src/drone/Drone.java
@@ -4,9 +4,13 @@ import java.util.Calendar;
 import java.util.Date;
 
 import location.Location;
+import warehouse.WarehouseManager;
 
 public class Drone {
 
+	private static int IDGenerator = 0;
+
+	private final int id;
 	private final int bateryUnits;
 	private final int chargingRate;
 	private final int weightCapacity;
@@ -15,6 +19,7 @@ public class Drone {
 	private Date timeWhenAvailable;
 
 	public Drone(int bateryUnits, int chargingRate, int weightCapacity, Location currentLocation) {
+		this.id = IDGenerator += 1;
 		this.bateryUnits = bateryUnits;
 		this.chargingRate = chargingRate;
 		this.weightCapacity = weightCapacity;
@@ -22,12 +27,56 @@ public class Drone {
 		currentBatery = bateryUnits;
 	}
 
-	public void makeDelivery(Location warehouseLocation, Location deliveryLocation, Date startTime) {
+	public Date makeDelivery(Location warehouseLocation, Location deliveryLocation, Date startTime) {
+		long estimatedDeliveryTime = Math.max(timeWhenAvailable.getTime(), startTime.getTime());
+		double distance = getDistance(warehouseLocation, deliveryLocation);
 
+		if (distance > currentBatery) {
+			estimatedDeliveryTime += ((distance - currentBatery) * chargingRate * 60000);
+			currentBatery = 0;
+		} else {
+			currentBatery -= distance;
+		}
+		currentLocation = WarehouseManager.getInstance().getNearestWarehouseLocation(deliveryLocation);
+
+		estimatedDeliveryTime += (distance * 60000);
+		timeWhenAvailable.setTime(estimatedDeliveryTime);
+
+		return new Date(estimatedDeliveryTime);
 	}
 
-	public Date estimateDeliveryTime() {
-		return null;
+	public Date estimateDeliveryTime(Location warehouseLocation, Location deliveryLocation, Date startTime) {
+		long estimatedDeliveryTime = Math.max(timeWhenAvailable.getTime(), startTime.getTime());
+		double distance = getDistance(warehouseLocation, deliveryLocation);
+
+		if (distance > currentBatery) {
+			estimatedDeliveryTime += ((distance - currentBatery) * chargingRate * 60000);
+		}
+
+		estimatedDeliveryTime += (distance * 60000);
+
+		return new Date(estimatedDeliveryTime);
+	}
+
+	/**
+	 * Calculate drone path distance.
+	 * <p>
+	 * Get warehouse and delivery location and calculate the distance: current
+	 * to warehouse to delivery to returning warehouse.
+	 * <p>
+	 */
+
+	private double getDistance(Location warehouseLocation, Location deliveryLocation) {
+		Location nearestWarehouseLocation = WarehouseManager.getInstance()
+				.getNearestWarehouseLocation(deliveryLocation);
+		double distance = currentLocation.getDistance(warehouseLocation);
+		distance += warehouseLocation.getDistance(deliveryLocation);
+		distance += deliveryLocation.getDistance(nearestWarehouseLocation);
+		return distance;
+	}
+
+	public int getId() {
+		return id;
 	}
 
 	public int getCurrentBatery() {
