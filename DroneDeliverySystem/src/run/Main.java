3fc041d85fb943a6837168cc9bf22688190a9e78
stoimenoff <stoimenoffjr@gmail.com>
2016-02-28 00:52:43 +0200

    Test file added to project.

--- DroneDeliverySystem/src/run/Main.java
+++ DroneDeliverySystem/src/run/Main.java
@@ -19,21 +19,19 @@ public class Main {
 		for (int i = 0; i < 30; i++) {
 			DroneManager.getInstance().addDrone(1200, 3, 200);
 		}
-		
+
 		/*
-		String sup = "supply 5 2016-10-25 12:32 tomato 5 100 potato 6 50 cheese 2 4";
-		String del = "delivery 4 2016-10-25 12:31 420,369 tomato 5 potato 20";
-		Request supply = Parser.parse(sup);
-		RequestManager.getInstance().submitRequest(supply);
-
-		Request delivery = Parser.parse(del);
-		RequestManager.getInstance().submitRequest(delivery);
-
-		System.out.println(Logger.getInstance().getRequestLogs(supply));
-
-		System.out.println(Logger.getInstance().getRequestLogs(delivery));
-		*/
-		File f = new File("/home/stoimenoff/Desktop/NewDrones/test");
+		 * String sup =
+		 * "supply 5 2016-10-25 12:32 tomato 5 100 potato 6 50 cheese 2 4";
+		 * String del = "delivery 4 2016-10-25 12:31 420,369 tomato 5 potato 20"
+		 * ; Request supply = Parser.parse(sup);
+		 * RequestManager.getInstance().submitRequest(supply); Request delivery
+		 * = Parser.parse(del);
+		 * RequestManager.getInstance().submitRequest(delivery);
+		 * System.out.println(Logger.getInstance().getRequestLogs(supply));
+		 * System.out.println(Logger.getInstance().getRequestLogs(delivery));
+		 */
+		File f = new File("test");
 		for (String requestString : RequestFileReader.readFile(f)) {
 			Request req = Parser.parse(requestString);
 			RequestManager.getInstance().submitRequest(req);
